9336c8d646840e7434d7c90233fea8e870616e48
saerom0 <jinxinsr@naver.com>
2022-12-08 01:27:31 +0900

    #visual 슬라이더 수정, #banner, compare, promo 추가

--- js/main.js
+++ js/main.js
@@ -1,42 +1,43 @@
 const header = document.querySelector("#header");
 const visual = document.querySelector("#visual");
-const middle = document.querySelector("#middle-menu");
+const middle = document.querySelector("#middle_menu");
+const banner = document.querySelector("#banner");
 
-const ulSlider = visual.querySelector(".main-bg");
-const sliders = visual.querySelectorAll(".main-bg li");
-const ulBtn = visual.querySelector(".bg-btn");
-const btns = visual.querySelectorAll(".bg-btn li");
+// swiper 사용
+// const slider_ul = visual.querySelector(".main_bg");
+// const bg_btn = visual.querySelectorAll(".bg_btn li");
 
+const tab_ul = middle.querySelector("#tab_btn ul");
+const tab_btn = middle.querySelectorAll("#tab_btn li");
+const tab_div = middle.querySelectorAll("#tab_btn div");
 
-const tab_ul = middle.querySelector("#tabBtn ul");
-const tab_btn = middle.querySelectorAll("#tabBtn li");
-const tab_div = middle.querySelectorAll("#tabBtn div");
+const bn_btn = banner.querySelectorAll("#banner li");
+const bn_div = banner.querySelectorAll("#banner div");
 
+// #visual 영역
+// bg_btn.forEach((el, idx) => {
+//     el.addEventListener("click", e => {
+//         e.preventDefault();
 
+//         let isOn = el.classList.contains("on");
+//         if (isOn) return;
 
-btns.forEach((el, idx) => {
-    el.addEventListener("click", e => {
-        e.preventDefault();
+//         activation(idx);
+//     })
+// });
 
-        let isOn = el.classList.contains("on");
-        if (isOn) return;
+// function activation(index) {
+//     new Anim(slider_ul, {
+//         prop: "margin-left",
+//         value: -100 * index + "%",
+//         duration: 1000,
+//     });
+//     for (let btn of bg_btn) { btn.classList.remove("on"); }
+//     bg_btn[index].classList.add("on");
 
-        activation(idx);
-    })
-});
+// }
 
-function activation(index) {
-    new Anim(ulSlider, {
-        prop: "margin-left",
-        value: -100 * index + "%",
-        duration: 1000,
-    });
-    for (let btn of btns) { btn.classList.remove("on"); }
-    btns[index].classList.add("on");
-
-}
-
-
+// #middle-menu영역
 tab_btn.forEach((el, idx) => {
     el.addEventListener("click", (e) => {
         e.preventDefault();
@@ -51,3 +52,29 @@ function init(arr, idx) {
     for (let el of arr) el.classList.remove("on");
     arr[idx].classList.add("on");
 }
+
+// #banner 영역
+bn_btn.forEach((el, idx) => {
+    el.addEventListener("click", e => {
+        e.preventDefault();
+
+        let isOn = el.classList.contains("on");
+        if (isOn) return;
+
+        activation(idx);
+    })
+});
+
+function activation(index) {
+    new Anim(bn_div, {
+        prop: "margin-left",
+        value: -100 * index + "%",
+        duration: 1000,
+    });
+    for (let btn of bn_btn) btn.classList.remove("on");
+    bn_btn[index].classList.add("on");
+
+}
+
+//visual swiper
+//swiper를 이쪽으로 옮기면 실행이 안됨
