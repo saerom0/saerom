05ff99136be9c2fc2e7e88512c92febfd763afd3
saerom0 <jinxinsr@naver.com>
2022-12-07 15:23:06 +0900

    탭추가

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 const header = document.querySelector("#header");
 const visual = document.querySelector("#visual");
-
-// const langBtn = header.querySelector(".lang a");
-// const lang = header.querySelectorAll(".lang ul li");
-// const currencyBtn = header.querySelector(".currency a");
-// const currency = header.querySelectorAll(".currency ul li");
+const middle = document.querySelector("#middle-menu");
 
 const ulSlider = visual.querySelector(".main-bg");
 const sliders = visual.querySelectorAll(".main-bg li");
 const ulBtn = visual.querySelector(".bg-btn");
 const btns = visual.querySelectorAll(".bg-btn li");
+
+
+const tab_ul = middle.querySelector("#tabBtn ul");
+const tab_btn = middle.querySelectorAll("#tabBtn li");
+const tab_div = middle.querySelectorAll("#tabBtn div");
+
+
 
 btns.forEach((el, idx) => {
     el.addEventListener("click", e => {
@@ -31,4 +34,20 @@ function activation(index) {
     for (let btn of btns) { btn.classList.remove("on"); }
     btns[index].classList.add("on");
 
+}
+
+
+tab_btn.forEach((el, idx) => {
+    el.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        init(tab_btn, idx);
+        init(tab_div, idx);
+    })
+})
+
+
+function init(arr, idx) {
+    for (let el of arr) el.classList.remove("on");
+    arr[idx].classList.add("on");
 }
