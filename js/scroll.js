6f2879d68798d060f9a28d7cbcfeddabc05f38a4
saerom0 <jinxinsr@naver.com>
2022-12-14 16:22:53 +0900

    #gallery위치 변경

--- js/scroll.js
+++ js/scroll.js
@@ -1,37 +1,105 @@
-const page = document.querySelectorAll(".page");
-// const quick = document.querySelectorAll(".quick li") 
-let pos_page = [];
-let up = -500;
-const cp_vid = document.querySelector('.cp_vid'); //비디오
+const pages = document.querySelectorAll(".page");
+const lis = document.querySelectorAll(".quick li")
+let posArr = [];
+let base = -500;
+// const cp_vid = document.querySelector('.cp_vid'); //비디오
 
-for (let el of page) {
-    pos_page.push(el.offsetTop);
+for (let el of pages) {
+    posArr.push(el.offsetTop);
 }
-console.log(pos_page);
-const ontop = document.querySelector(".on_top");
+console.log(posArr);
+// const ontop = document.querySelector(".on_top");
+
+// window.addEventListener("scroll", () => {
+//     let scroll = window.scrollY || window.pageYOffset;
+
+//     ontop.innerText = scroll;
+
+//     //3단메뉴가 상단에 붙는 순간부터 항상 상위에 고정(동작이 안됨)
+//     if (scroll >= pos_page[1]) {
+//         page[1].closest("ul").classList.add("on");
+//     } else {
+//         page[1].closest("ul").classList.remove("on");
+//     }
+// })
 
 window.addEventListener("scroll", () => {
     let scroll = window.scrollY || window.pageYOffset;
 
-    ontop.innerText = scroll;
-    if (scroll >= pos_page[1]) {
-        page[1].closest("ul").classList.add("on");
-    } else {
-        page[1].closest("ul").classList.remove("on");
-    }
+    pages.forEach((el, index) => {
+        if (scroll >= posArr[index] + base) {
+            // for (let el of lis) el.classList.remove("on");
+            // lis[index].classList.add("on");
 
-
-    // page.forEach((el, index) => {
-    //     if (scroll >= pos_page[index]) {
-    //         page.forEach((el) => el.classList.remove("on"))
-    //         el.classList.add("on");
-    //     }
-
-    //     //3단메뉴가 상단에 붙는 순간부터 항상 상위에 고정
-
-    // })
+            //모든 li, section을 비활성화
+            lis.forEach((el, index) => {
+                el.classList.remove("on");
+                pages[index].classList.remove("on");
+            })
+            //해당 순번의 li와 section을 활성화
+            lis[index].classList.add('on');
+            pages[index].classList.add("on");
+            // el.classList.add("on");      
+        }
+    })
 
 })
+
+//ul li를 클릭하면 해당 순번의 section으로 이동
+//이쪽은 클릭으로 스크롤을 이동시키는 쪽
+lis.forEach((el, index) => {
+    el.addEventListener("click", (e) => {
+        new Anim(window, {
+            prop: "scroll",
+            value: posArr[index],
+            duration: 500,
+        });
+
+        active(lis, index);
+        //모든 버튼을 반복을 돌면서 on을 제거하여 비활성화
+        for (let el of lis) el.classList.remove("on");
+        //클릭한 버튼만 on을 추가하여 활성화
+        el.classList.add("on");
+        // e.currentTarget.classList.add("on");
+
+    })
+})
+
+function active(arr, index) {
+    for (let el of arr) el.classList.remove("on");
+    arr[index].classList.add("on");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //gallery 카운터 함수
 function counter(el, num, time) {
@@ -57,20 +125,3 @@ counter(".record_3", 100, 2000);
 counter(".record_4", 9700, 1000);
 
 
-// lis.forEach((el, index) => {
-//     el.addEventListener("click", (e) => {
-//         new Anim(window, {
-//             prop: "scroll",
-//             value: posArr[index],
-//             duration: 500,
-//         });
-
-//         active(lis, index);
-//         //모든 버튼을 반복을 돌면서 on을 제거하여 비활성화
-//         for (let el of lis) el.classList.remove("on");
-//         //클릭한 버튼만 on을 추가하여 활성화
-//         el.classList.add("on");
-//         // e.currentTarget.classList.add("on");
-
-//     })
-// })
