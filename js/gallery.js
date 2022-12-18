6a7e112cfe2c754cdd0e9b7632bd67ead15475b4
saerom0 <jinxinsr@naver.com>
2022-12-18 14:12:41 +0900

    youtube api 추가

--- js/gallery.js
+++ js/gallery.js
@@ -1,22 +1,22 @@
-const base = "https://www.flickr.com/services/rest/?";
-const method = "flickr.interestingness.getList";
-const key = "2a1a0aebb34012a99c23e13b49175343";
-const per_page = 500;
-const format = "json";
+// const base = "https://www.flickr.com/services/rest/?";
+// const method = "flickr.interestingness.getList";
+// const key = "2a1a0aebb34012a99c23e13b49175343";
+// const per_page = 500;
+// const format = "json";
 
-const url = `${base}method=${method}&api_key=${key}&per_page=${per_page}&format=${format}&nojsoncallback=1`;
+// const url = `${base}method=${method}&api_key=${key}&per_page=${per_page}&format=${format}&nojsoncallback=1`;
 
-//해당 url값으로 비동기식 데이터 호출
-fetch(url)
-    .then((data) => {
+// //해당 url값으로 비동기식 데이터 호출
+// fetch(url)
+//     .then((data) => {
 
-        console.log(data);// 가져온 데이터 전체를 보여줌
-        let result = data.json(); // 가져온 데이터 중에 json형태의 값으로 변환함
-        console.log(result); //결과로 만들어진 데이터를 보여준다
-        return result; //해당 결과를 리턴(반환)해줘야 쓸수있다
-    })
-    .then((json) => {
-        // 반환된 값을 json이라는 매개변수로 받은뒤
-        let items = json.photos.photo; //콘솔에서 본것처럼 그안의 photos안의 photo로 접근함
-        console.log(items); // 500장의 사진이 json객체배열로 가져와짐
-    })
+//         console.log(data);// 가져온 데이터 전체를 보여줌
+//         let result = data.json(); // 가져온 데이터 중에 json형태의 값으로 변환함
+//         console.log(result); //결과로 만들어진 데이터를 보여준다
+//         return result; //해당 결과를 리턴(반환)해줘야 쓸수있다
+//     })
+//     .then((json) => {
+//         // 반환된 값을 json이라는 매개변수로 받은뒤
+//         let items = json.photos.photo; //콘솔에서 본것처럼 그안의 photos안의 photo로 접근함
+//         console.log(items); // 500장의 사진이 json객체배열로 가져와짐
+//     })
---
+++ js/youtube.js
@@ -0,0 +1,68 @@
+const vidList = document.querySelector(".videoList");
+const key = 'AIzaSyCwLGRESuf8Zwcy3A9ufGkTyoUcRtYEu_Y';
+const playlistId = 'PLKfwPBQMIxKDP60C-MiQuif3-9MB7RTHm';
+const num = 9;
+const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlistId}&maxResults=${num}`;
+
+fetch(url)
+    .then((data) => {
+        return data.json();
+    })
+    .then((json) => {
+        let items = json.items;
+        console.log(items);
+        let result = "";
+
+        items.map((el) => {
+            let title = el.snippet.title;
+
+            if (title.length > 50) {
+                title = title.substr(0, 50) + "...";
+            }
+
+            let con = el.snippet.description;
+            if (con.length > 90) {
+                con = con.substr(0, 90) + "...";
+            }
+            let date = el.snippet.publishedAt;
+            date = date.split("T")[0];
+
+            result += `
+            <article>
+                <a href="${el.snippet.resourceId.videoId}" class="pic">
+                    <img src="${el.snippet.thumbnails.medium.url}">
+                </a>
+                <div class="con">
+                    <h2>${title}</h2>
+                    <p>${con}</p>
+                    <span>Link date : ${date}</span>
+                </div>
+            </article>
+            `;
+        })
+
+        vidList.innerHTML = result;
+    })
+
+vidList.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (!e.target.closest("a")) return;
+
+    const vidId = e.target.closest("article").querySelector("a").getAttribute("href");
+
+    let pop = document.createElement("figure");
+    pop.classList.add("pop");
+    pop.innerHTML = `
+        <iframe src="https://www.youtube.com/embed/${vidId}" frameborder="0" width="100%" height="100%" allowfullscreen></iframe>
+        <span class="btnClose">close</span>
+        `;
+    vidList.append(pop);
+})
+
+vidList.addEventListener("click", (e) => {
+    const pop = vidList.querySelector(".pop");
+    if (pop) {
+        const close = pop.querySelector("span");
+        if (e.target == close) pop.remove();
+    }
+})
