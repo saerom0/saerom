5a28823410979c848046a0e14f2d66fb69f7adf3
saerom0 <jinxinsr@naver.com>
2023-02-03 23:36:13 +0900

    파일명 변경

--- js/main.js
+++ js/main.js
@@ -82,3 +82,22 @@ function activate(index) {
 // 	n += 1;
 // }
 // promo.innerHTML = tags1;
+
+// //about
+// const tab_btn = document.querySelectorAll('#service_type li');
+// const tab_contents = document.querySelectorAll('#service_type article');
+
+// tab_btn.forEach((el, index) => {
+// 	el.addEventListener('click', (e) => {
+// 		let isOn = e.currentTarget.classList.contains('on');
+// 		if (isOn) return;
+
+// 		act(tab_btn, index);
+// 		act(tab_contents, index);
+// 	});
+// });
+
+// function act(arr, index) {
+// 	for (let el of arr) el.classList.remove('on');
+// 	arr[index].classList.add('on');
+// }
